391ee126daecbfe6f394f512bfdfdc9e066d735c
knikolov <kami_985@abv.bg>
2022-03-01 16:31:34 +0200

    User component
    Implementation and Testing

---
+++ src/components/user/User.jsx
@@ -0,0 +1,26 @@
+import Container from "@mui/material/Container";
+import styles from "./User.module.scss";
+import classNames from "classnames";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+
+
+export default function User({avatar,name,size}){
+
+    return (
+    <div className={classNames(styles["user-container"])}>
+    <Container maxWidth="xs" > 
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid item xs={2}>
+                <Avatar src={avatar} sx={{width: {size},height:{size}}}></Avatar>
+            </Grid>
+            <Grid item xs={4}> 
+            <Typography variant="h4">{name}</Typography>
+            </Grid>
+        </Grid>
+    </Container>
+    </div>
+    );
+
+}
